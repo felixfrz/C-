60c184ed65fb5119afb72a983dd3a25aedec4135
Ayodeji <oladoyinayodeji@gmail.com>
2023-12-25 08:58:16 +0000

    Explicit Casting

---
+++ ExplicitCasting/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //int
+        int a = 500;
+
+        //float
+        //float b;
+
+        //byte
+        byte b;
+
+
+        //int to float
+        //b = (float)a;
+        b = (byte)a;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.ReadKey();
+    }
+}
