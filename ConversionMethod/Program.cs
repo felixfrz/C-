434c5cacd8087dcdbdbad5f0a17e944cfeb2cceb
Ayodeji <oladoyinayodeji@gmail.com>
2023-12-25 09:20:15 +0000

    Conversion Method

---
+++ ConversionMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //int
+        int a = 1000;
+
+        //string
+        string b;
+
+        //int --> string
+        b = Convert.ToString(a);
+
+        Console.WriteLine(a);// --> 1000 as int
+        Console.WriteLine(b);// ---> 1000 as string
+
+        Console.ReadKey();
+    }
+}
