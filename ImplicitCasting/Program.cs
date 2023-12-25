13d0d1d6c392e124427260d95364c5c784011cb7
Ayodeji <oladoyinayodeji@gmail.com>
2023-12-25 05:51:50 +0000

    Implicit Casting

---
+++ ImplicitCasting/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //sbyte
+        sbyte a = 10;
+
+        //int
+        int b;
+
+        //copying the value from 'a' to 'b'
+
+        b = a;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        Console.ReadKey();
+    }
+}
