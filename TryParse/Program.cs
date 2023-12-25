4a1e45f9ed6852305b54de2214e798d705f40c4b
Ayodeji <oladoyinayodeji@gmail.com>
2023-12-25 09:14:19 +0000

    TryParse

---
+++ TryParse/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // read in from the keyboard
+        string s;
+
+        Console.WriteLine("Enter a number: ");
+        s = Console.ReadLine();
+
+        //TryParse
+       bool b =  int.TryParse(s, out int n);
+
+        if(b == true)
+        {
+            Console.WriteLine("Conversion is successful");
+            Console.WriteLine(n);
+        }
+        else
+        {
+            Console.WriteLine("Conversion failed");
+        }
+
+        Console.ReadKey();
+    }
+}
