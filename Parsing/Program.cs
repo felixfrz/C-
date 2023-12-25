87fbc3d8b95869761874c95996b3fc7f26dc0f23
Ayodeji <oladoyinayodeji@gmail.com>
2023-12-25 09:06:17 +0000

    Parsing

---
+++ Parsing/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //string
+        string a = "100";
+
+        //int
+        int b;
+
+
+
+        //string to int
+       b =  int.Parse(a);
+
+        Console.WriteLine(a); //Output: "100" as string
+        Console.WriteLine(b); //Output "100" as int
+
+
+        Console.ReadKey();
+    }
+}
