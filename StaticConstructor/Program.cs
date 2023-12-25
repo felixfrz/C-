e205073358dd3cb4f4e4085c0c83f1e2df7a1af0
Ayodeji <oladoyinayodeji@gmail.com>
2023-12-25 10:57:54 +0000

    Static Constructor

---
+++ StaticConstructor/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //create theree object for Employee
+        Employee emp1 = new Employee(101, "Frz", "Manager");
+        Employee emp2 = new Employee(102, "Bim", "Asst. Manager");
+        Employee emp3 = new Employee(103, "Scott", "Clerk");
+
+        //display fields
+        Console.WriteLine(Employee.companyName);
+        Console.WriteLine("\nFirst Employee: ");
+        Console.WriteLine(emp1.empID);
+        Console.WriteLine(emp1.empName);
+        Console.WriteLine(emp1.job);
+
+        Console.WriteLine("Second Employee: ");
+        Console.WriteLine(emp2.empID);
+        Console.WriteLine(emp2.empName);
+        Console.WriteLine(emp2.job);
+
+        Console.WriteLine("Third Employee: ");
+        Console.WriteLine(emp3.empID);
+        Console.WriteLine(emp3.empName);
+        Console.WriteLine(emp3.job);
+
+        Console.ReadKey();
+    }
+}
