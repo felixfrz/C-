796744f85df23d988a1657e62e631cd3e71e6e3f
Ayodeji <oladoyinayodeji@gmail.com>
2023-12-25 03:12:50 +0000

    Params Example

---
+++ RefExample/Program.cs
@@ -0,0 +1,32 @@
+﻿class Student
+{
+
+    public void DisplaySubjects(params string[] subjects)
+    {
+
+        //foreach (string i in subjects)
+        //{
+
+        //    Console.WriteLine(i);
+        //}
+
+        for (int i = 0; i < subjects.Length; i++)
+        {
+            Console.WriteLine(subjects[i]);
+        }
+
+        Console.ReadKey();
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+
+
+        s.DisplaySubjects("ffdddg", "hdgdggdd", "dcdfdd", "frrddtdt", "etefy", "teefe");
+    }
+}
