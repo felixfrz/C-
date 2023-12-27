18df85ddcac8949222ee6ec0ec8d2dea7efa2a88
Ayodeji <oladoyinayodeji@gmail.com>
2023-12-27 14:47:23 +0000

    Begn

---
+++ Beginners/Program.cs
@@ -0,0 +1,31 @@
+﻿using Beginners;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<User> users = new List<User>();
+
+
+        User user1 = new User("frz", 23);
+        User user2 = new User("Deji", 63);
+        User user3 = new User("Ayo", 93);
+
+        users.Add(user1);
+        users.Add(user2);
+        users.Add(user3);
+
+     
+
+
+        foreach (var user in users)
+        {
+          
+
+        
+            Console.WriteLine(user.Name);
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Beginners/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Beginners
+{
+    public class User
+    {
+
+        private string name;
+        private int age;
+        public User(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name
+        {
+            set {
+
+                if(value.Length > 1)
+                {
+                    name = value;
+                }
+        
+
+            }
+            get { return name; }
+        }
+
+        public int Age
+        {
+            set {
+
+                age = value;
+
+            }
+            get {
+                return age;
+            }
+        }
+    }
+}
+
