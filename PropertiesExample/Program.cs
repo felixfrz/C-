a6007423fcd98820d35d02b990b5398de43936c8
Ayodeji Oladoyin <oladoyinayodeji@gmail.com>
2024-01-12 12:47:12 +0000

    Readyonly and Writeonly properties

--- PropertiesExample/Program.cs
+++ PropertiesExample/Program.cs
@@ -4,6 +4,7 @@
     {
         //create theree object for Employee
         Employee emp1 = new Employee(101, "Frz", "Manager");
+        emp1.Tax = 50;
         Employee emp2 = new Employee(102, "Bim", "Asst. Manager");
         Employee emp3 = new Employee(103, "Scott", "Clerk");
         Employee emp4 = new Employee() { EmpName = "Ford", Job = "CEO" };
@@ -14,21 +15,30 @@
         Console.WriteLine(emp1.EmpID);
         Console.WriteLine(emp1.EmpName);
         Console.WriteLine(emp1.Job);
+        Console.WriteLine(emp1.Salary);
+        Console.WriteLine(emp1.CalculateNetSalary());
+        //emp1.Salary = 29944;
+        Console.WriteLine();
 
         Console.WriteLine("Second Employee: ");
         Console.WriteLine(emp2.EmpID);
         Console.WriteLine(emp2.EmpName);
         Console.WriteLine(emp2.Job);
+        Console.WriteLine(emp2.Salary);
+        Console.WriteLine();
 
         Console.WriteLine("Third Employee: ");
         Console.WriteLine(emp3.EmpID);
         Console.WriteLine(emp3.EmpName);
         Console.WriteLine(emp3.Job);
+        Console.WriteLine(emp2.Salary);
+        Console.WriteLine();
 
         Console.WriteLine("Fourth Employee: ");
         Console.WriteLine(emp4.EmpID);
         Console.WriteLine(emp4.EmpName);
         Console.WriteLine(emp4.Job);
+        Console.WriteLine(emp2.Salary);
 
         Console.ReadKey();
     }
--- PropertiesExampleClass/Class1.cs
+++ PropertiesExampleClass/Class1.cs
@@ -4,6 +4,8 @@
     private int _empID;
     private string _empName;
     private string _job;
+    private double _salary;
+    private double _tax;
 
 
     //instance property
@@ -56,22 +58,52 @@
         this._empID = empID;
         this._empName = empName;
         this._job = job;
+        this._salary = 1000;
     }
     // constructor 2
     public Employee(int empID, string empName)
     {
         this._empID = empID;
         this._empName = empName;
+        this._salary = 1000;
     }
 
     // constructor 3
     public Employee()
     {
         _empID = 1;
+        this._salary = 1000;
     }
     //static constructor
     static Employee()
     {
         _companyName = "ABC Industries";
+    }
+
+    //Ready-only Property
+    public double Salary
+    {
+        get { return _salary; }
+
+    }
+
+    //Writeonly Property
+    public double Tax
+    {
+        set
+        {
+
+            if (value >= 0 && value <= 100)
+            {
+                _tax = value;
+            }
+        }
+    }
+
+    //method
+    public double CalculateNetSalary()
+    {
+        double t = Salary - _tax;
+        return t;
     }
 }
