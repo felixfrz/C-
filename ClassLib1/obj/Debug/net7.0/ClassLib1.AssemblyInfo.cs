32ec0f2e5812db4aed957420a8d51b3ec74a9ed5
Ayodeji Oladoyin <oladoyinayodeji@gmail.com>
2024-01-18 20:18:48 +0000

    Inheritance

--- ClassLib1/obj/Debug/net7.0/ClassLib1.AssemblyInfo.cs
+++ ClassLib1/obj/Debug/net7.0/ClassLib1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLib1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4894ffa2e1c41a149442759843c13e8e175e603d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cef47ab90067317d433045f2d22e9aa166894ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLib1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLib1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HelloWorld/obj/Debug/net7.0/First.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net7.0/First.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4894ffa2e1c41a149442759843c13e8e175e603d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cef47ab90067317d433045f2d22e9aa166894ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("First")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
