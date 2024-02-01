02bff448739206676c958a37d45844065faf2211
Ayodeji Oladoyin <oladoyinayodeji@gmail.com>
2024-02-01 19:35:08 +0000

    Generic Stack

---
+++ Generic Stack/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+var countries = new Stack<string>();
+
+countries.Push("Nigeria");
+countries.Push("Ghana");
+countries.Push("S.Africa");
+
+
+countries.Pop();
+
+
+foreach (var country in countries)
+{
+    Console.WriteLine(country);
+}
+
+Console.ReadKey();
--- HelloWorld/obj/Debug/net7.0/First.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net7.0/First.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbba24aaab15d574fa2bfc453a5b9502d56cde24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b0ce4657192b60136ddcc580efd231ceb6e3c02")]
 [assembly: System.Reflection.AssemblyProductAttribute("First")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
