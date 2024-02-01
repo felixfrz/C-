7a4d59c7ec21a07b62aea8c9c2267539512f12fd
Ayodeji Oladoyin <oladoyinayodeji@gmail.com>
2024-02-01 17:52:35 +0000

    Interfaces

--- ClassLib1/obj/Debug/net7.0/ClassLib1.AssemblyInfo.cs
+++ ClassLib1/obj/Debug/net7.0/ClassLib1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLib1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de13039d15e43c861a7ea61245a6009159b888e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e143b5594078d05b82ac80667981b1211e03ad4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLib1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLib1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
