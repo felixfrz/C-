cbba24aaab15d574fa2bfc453a5b9502d56cde24
Ayodeji Oladoyin <oladoyinayodeji@gmail.com>
2024-02-01 18:35:04 +0000

    Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,58 +1,76 @@
 ﻿
-ICar tesla = new TeslaModelS();
-ICar nissan = new NissanMicra();
-Console.WriteLine($"{tesla.GetModel()} : {tesla.GetTopSpeed()} km/h");
-Console.WriteLine($"{nissan.GetModel()} : {nissan.GetTopSpeed()} km/h");
+
+
+//Arrays and Collections
+
+
+int[] array1 = new int[4];
+
+array1[0] = 7;
+array1[1] = 6;
+array1[2] = 5;
+array1[3] = 4;
+
+Console.WriteLine(array1);
+Console.WriteLine(array1[0]);
+
+//Array initializer
+int[] array2 = new int[] { 34, 67, 86, 3 };
+
+var array3 = new int[] { 09, 45, 97, 23 };
+
+Console.WriteLine(array2[1]);
+Console.WriteLine(array3[1]);
+
+//Alternative Syntax
+int[] array4 = { 657, 959, 23, 89 };
+Console.WriteLine(array4[2]);
+
+
+var p1 = new Person[2];
+p1[0] = new Person("frz");
+p1[1] = new Person("demo");
+
+
+Person[] p2 = new Person[2];
+p2[0] = new Person("yoshi");
+p2[1] = new Person("ryu");
+
+
+Person[] p3 = { new Person("des"), new Person("ken") };
+p3[0].Name = "Test";
+
+//for loop
+for (int i = 0; i < p2.Length; i++)
+{
+    Console.WriteLine($"Name: {p2[i].Name}");
+}
+
+//foreach
+foreach (Person p in p1)
+{
+    Console.WriteLine(p.Name);
+}
+
+//While loop
+int j = 0;
+while (j < p3.Length)
+{
+    Console.WriteLine(p3[j].Name);
+    j++;
+}
+
+
+
 Console.ReadKey();
 
 
-public interface ICar
+public class Person
 {
-    string GetModel();
+    public string Name { get; set; }
 
-    int GetTopSpeed();
-}
-
-
-public class TeslaModelS : ICar
-{
-    public string GetModel()
+    public Person(string name)
     {
-        return "Tesla Model S";
-    }
-
-    public int GetTopSpeed()
-    {
-        return 282;
+        Name = name;
     }
 }
-
-public class NissanMicra : ICar
-{
-    public string GetModel()
-    {
-        return "Nissan Micra";
-    }
-
-    public int GetTopSpeed()
-    {
-        return 186;
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
