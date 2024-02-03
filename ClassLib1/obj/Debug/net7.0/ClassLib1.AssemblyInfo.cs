bfa10e59972fbb7508828da51460efb79e51c04c
Ayodeji Oladoyin <oladoyinayodeji@gmail.com>
2024-02-03 22:20:10 +0000

    LINQ Select

--- ClassLib1/obj/Debug/net7.0/ClassLib1.AssemblyInfo.cs
+++ ClassLib1/obj/Debug/net7.0/ClassLib1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLib1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898926c01205bae62f45bda717f02ac3ec9cd84c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955e58ad12cb1c949e6e7a3e5feee2fbf39b6330")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLib1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLib1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ LINQSelect/Program.cs
@@ -0,0 +1,39 @@
+﻿class Employee
+{
+    public int EmpID { get; set; }
+    public string EmpName { get; set; }
+    public string Job { get; set; }
+    public double Salary { get; set; }
+}
+
+class Person
+{
+    public string PersonName { get; set; }
+}
+public class Program
+{
+
+    private static void Main(string[] args)
+    {
+
+        //Collection of objects
+        List<Employee> employees =
+        [
+            new Employee() { EmpID = 101, EmpName = "Henry", Job = "Designer", Salary = 7232 },
+            new Employee() { EmpID = 102, EmpName = "Jack", Job = "Developer", Salary = 4500 },
+            new Employee() { EmpID = 103, EmpName = "Gabriel", Job = "Analyst", Salary = 1293 },
+            new Employee() { EmpID = 104, EmpName = "William", Job = "Manager", Salary = 2873 },
+            new Employee() { EmpID = 105, EmpName = "Alexa", Job = "Manager", Salary = 6232 },
+            new Employee() { EmpID = 106, EmpName = "Jessica", Job = "Manager", Salary = 4545 }
+        ];
+
+        //Select
+        List<Person> result = employees.Select((employee) => new Person { PersonName = employee.EmpName }).ToList();
+        foreach (Person i in result)
+        {
+            Console.WriteLine(i.PersonName);
+        }
+
+        Console.ReadKey();
+    }
+}
