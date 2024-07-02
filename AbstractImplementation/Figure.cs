033965efd3a8700f846c783c8a866b36537f7657
felixfrz <oladoyinayodeji@gmail.com>
2024-07-02 08:52:40 +0100

    Add files via upload
    AbstractImplementation

---
+++ AbstractImplementation/Figure.cs
@@ -0,0 +1,66 @@
+﻿namespace AbstractImplementation
+{
+    public abstract class Figure
+    {
+        public double WIdth, Height, Radius;
+        public const float Pi = 3.14f;
+        public abstract double GetArea();
+    }
+
+    public class Rectangle : Figure
+    {
+        public Rectangle(double Width, double Height)
+        {
+            this.WIdth = Width;
+            this.Height = Height;
+        }
+
+        public override double GetArea()
+        {
+            return WIdth * Height;
+        }
+    }
+
+
+    public class Circle : Figure
+    {
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+        }
+
+        public override double GetArea()
+        {
+            return Pi * Radius * Radius;
+        }
+    }
+
+    public class Cone : Figure
+    {
+        public Cone(double Radius, double Height)
+        {
+            this.Radius = Radius;
+            this.Height = Height;
+        }
+        public override double GetArea()
+        {
+            return Pi * Radius * (Radius + Math.Sqrt(Height * Height + Radius * Radius));
+        }
+    }
+    class TestFigures
+    {
+        static void Main()
+        {
+            Rectangle r = new Rectangle(12.67, 56.78);
+            Circle c = new Circle(45.67);
+            Cone cn = new Cone(34.98, 12.98);
+
+
+            Console.WriteLine("Area of Rectangle: " + r.GetArea());
+            Console.WriteLine("Area of Circle: " + c.GetArea());
+            Console.WriteLine("Area of Cone: " + cn.GetArea());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AbstractImplementation/Shape.cs
@@ -0,0 +1,74 @@
+﻿using AbstractImplementation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractImplementationn
+{
+    internal abstract class Shape
+    {
+
+        public double Width, Height;
+        public const double Pi = 1.42f;
+        public abstract double GetArea();
+    }
+
+    internal class Rectangle:Shape
+    {
+        public Rectangle(double Width, double Height)
+        {
+            base.Width = Width;
+            base.Height = Height;
+           
+        }
+
+        public override double GetArea() 
+        {
+            return Width * Height;
+        }
+    }
+
+    internal class Circle:Shape
+    {
+        public Circle(double Width)
+        {
+            base.Width=Width;
+        }
+
+        public override double GetArea()
+        {
+            return Pi * Width;
+        }
+    }
+
+
+    internal class Run()
+    {
+        static void Main()
+        {
+            Rectangle r = new Rectangle(12.21, 9.22);
+            Circle c = new Circle(3.24);
+
+              
+
+            Console.WriteLine(r.GetArea());
+            Console.WriteLine(c.GetArea());
+
+            Console.ReadLine();
+
+                    //Friday 26 April 2024
+
+                    //15:35 Item Received
+                    //      South Midlands MC
+                    //00:39 Item Received
+                    //      Princess Royal Parcel Hub
+
+                    //Saturday 20 April 2024
+                    //12:03 Sender has despatched item
+        }
+    }
+}
