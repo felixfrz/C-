fdb9198678ee38fcde3eb2c6563b7149b55df139
Ayodeji Oladoyin <oladoyinayodeji@gmail.com>
2024-07-03 04:50:39 +0100

    Chaining Tasks using Continuation Tasks

---
+++ TPLConsole/Class10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLConsole
+{
+    internal class Class10
+    {
+        static void Method1(int x, int ub)
+        {
+            for (int i = 1; i <= ub; i++)
+                Console.WriteLine($"{x} * {i} = {x * i}");
+        }
+        static void Method2(int x, int ub)
+        {
+            for (int i = ub; i > 0; i--)
+                Console.WriteLine($"{x} * {i} = {x * i}");
+        }
+        static void Main()
+        {
+            Task t = Task.Factory.StartNew(() => Method1(5, 12)).ContinueWith((antecedent) => Console.WriteLine()).ContinueWith((antecedent) => Method2(5, 12));
+            t.Wait();
+            Console.ReadLine();
+        }
+
+    }
+}
