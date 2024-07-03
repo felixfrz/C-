4a1ef914bd9e381455f4e07325b9eabf75de25df
Ayodeji Oladoyin <oladoyinayodeji@gmail.com>
2024-07-03 03:57:10 +0100

    Parallel Equivalent

---
+++ TPLConsole/Class9.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLConsole
+{
+    internal class Class9
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50 };
+
+
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+            foreach (int i in arr)
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; i value: {i}");
+            }
+            sw1.Stop();
+
+            Console.WriteLine();
+
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+            Parallel.ForEach(arr, i =>
+            {
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; i value: {i}");
+            });
+            sw2.Stop();
+
+            Console.WriteLine("Time taken to execute code by using sequential foreach loop: " + sw1.ElapsedMilliseconds);
+            Console.WriteLine("Time taken to execute code by using parallel foreach loop: " + sw2.ElapsedMilliseconds);
+
+
+
+
+        }
+    }
+}
