9cc044c569c54747416cb25c856043e0decbb2db
Ayodeji Oladoyin <oladoyinayodeji@gmail.com>
2024-07-03 02:11:28 +0100

    Calling value returning methods with-out parameters by using Tasks:

--- TPLConsole/Class2.cs
+++ TPLConsole/Class2.cs
@@ -20,6 +20,8 @@ namespace TPLConsole
             for (int i = 1; i <= 100; i++)
             {
                 Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}; Print2 Method: {i}");
+                if(i == 50)
+                    Thread.Sleep(10000);
             }
         }
         static void Print3()
@@ -31,12 +33,19 @@ namespace TPLConsole
         }
         static void Main(string[] args)
         {
-            Task t1 = new Task(Print1);
-            Task t2 = new Task(Print2);
-            Task t3 = new Task(Print3);
-            t1.Start(); t2.Start(); t3.Start();
-            t1.Wait(); t2.Wait(); t3.Wait();
+            //Task t1 = new Task(Print1);
+            //Task t2 = new Task(Print2);
+            //Task t3 = new Task(Print3);
+            //t1.Start(); t2.Start(); t3.Start();
+            //t1.Wait(); t2.Wait(); t3.Wait();
+            //Console.WriteLine($"Main thread with Id: {Thread.CurrentThread.ManagedThreadId} is exiting.");
+            // OR
+            Task t1 = Task.Factory.StartNew(Print1);
+            Task t2 = Task.Factory.StartNew(Print2);
+            Task t3 = Task.Factory.StartNew(Print3);
+            Task.WaitAll(t1, t2, t3);
             Console.WriteLine($"Main thread with Id: {Thread.CurrentThread.ManagedThreadId} is exiting.");
+
 
         }
 
---
+++ TPLConsole/Class3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLConsole
+{
+    internal class Class3
+    {
+        static int GetLength()
+        {
+            string str = "";
+            for (int i = 1; i <= 100000; i++)
+            {
+                str += i;
+            }
+            return str.Length;
+        }
+        static string ToUpper()
+        {
+            string str = "Hello World";
+            return str.ToUpper();
+        }
+        static void Main()
+        {
+            //Task<int> t1 = new Task<int>(GetLength);
+            //Task<string> t2 = new Task<string>(ToUpper);
+            //t1.Start(); t2.Start();
+            // OR
+            Task<int> t1 = Task.Factory.StartNew(GetLength);
+            Task<string> t2 = Task.Factory.StartNew(ToUpper);
+
+            int Result1 = t1.Result;
+            string Result2 = t2.Result;
+            Console.WriteLine($"Value of Result1 is: {Result1}");
+            Console.WriteLine($"Value of Result2 is: {Result2}");
+        }
+
+    }
+}
